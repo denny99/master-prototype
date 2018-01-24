beac2657ed24ba06bf7187210214b7309366aa81
Denny Müller <>
2018-01-24 11:51:05 +0100

    removed invalid hybrid

--- src/main/java/de/uni/frankfurt/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package de.uni.frankfurt.exceptions;
-
-public class ResourceNotFoundException extends RestException {
-  public ResourceNotFoundException(String id, Class clazz) {
-    super(404, "Resource not found: " + id, clazz);
-  }
-}
--- src/main/java/de/uni/frankfurt/webservice/WSApplication.java
+++
@@ -1,9 +0,0 @@
-package de.uni.frankfurt.webservice;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class WSApplication extends Application {
-
-}
