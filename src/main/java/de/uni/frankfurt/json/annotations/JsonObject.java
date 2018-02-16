66f9c004bcf26d5531ce17e596b3133c5eaea731
Denny Müller <>
2018-02-16 21:16:16 +0100

    annotations

---
+++ src/main/java/de/uni/frankfurt/json/annotations/JsonObject.java
@@ -0,0 +1,14 @@
+package de.uni.frankfurt.json.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * annotate the actual fields to set json schema props
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface JsonObject {
+}
